1c31ec17b4c9e7d5e016c6157a5a21d192d9080e
bansipatel1405 <patelbansi1405@gmail.com>
2023-05-17 12:32:44 +0530

    added
    added

---
+++ Advance Javascript/react(modual 6)/lazy loading/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+// import About from './component/About';
+// import Home from './component/Home';
+const Home = lazy(()=>import('./component/Home'))
+const About = lazy(()=>import('./component/About'))
+import React,{Suspense,lazy} from 'react';
+
+function App() {
+  return (
+    <>
+     <div className='text-center mt-5'>
+            <Home/>
+            <Suspense fallback = {<div>LOADING......</div>}>
+              <About/>
+            </Suspense>
+      </div>
+    </>
+  );
+}
+
+export default App;
